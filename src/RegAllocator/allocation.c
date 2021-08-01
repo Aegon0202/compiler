27e48e8d2fbf8523c2de495ed6f14611ed94d07e
nicolasios <1498913522@qq.com>
2021-08-01 23:52:52 +0800

    modify

--- src/RegAllocator/allocation.c
+++ src/RegAllocator/allocation.c
@@ -9,16 +9,37 @@ void walkIntervals(IntervalList* unhandled_list) {
 
     MALLOC(active_list, IntervalList, 1);
     MALLOC(inactive_list, IntervalList, 1);
+    MALLOC(handled_list, IntervalList, 1)
+    handled_list->value = 0;
     active_list->value = 0;
     inactive_list->value = 0;
     list_entry_t* active_list_head = &active_list->link;
     list_entry_t* inactive_list_head = &inactive_list->link;
+    list_entry_t* handled_list_head = &handled_list->link;
     list_init(active_list_head);
     list_init(inactive_list_head);
+    list_init(handled_list_head);
 
+    //to be modified
     while (!list_empty(unhandled_list_head)) {
         list_entry_t* first = list_next(unhandled_list_head);
         IntervalList* currrent = le2struct(first, IntervalList, link);
         int position = le2struct(list_next(currrent->value->range_list), RangeList, link)->begin;
+        while (!list_empty(active_list_head)) {
+            //获取当前active_list第一个iterval
+            IntervalList* itList = le2struct(list_next(active_list_head), IntervalList, link);
+            Interval* it = itList->value;
+            //获取当前Interval的最后一个range的后部区间
+            int itposition = le2struct(list_next(it->range_list), RangeList, link)->begin;
+            if (itposition < position) {
+                MoveitFromAtoB(unhandled_list_head, active_list_head, itList);
+            } else if (1) {
+            }
+        }
     }
+}
+
+void MoveitFromAtoB(list_entry_t* A, list_entry_t* B, IntervalList* itList) {
+    list_del(&itList->link);
+    list_add_after(B, &itList->link);
 }
--- src/RegAllocator/allocation.h
+++ src/RegAllocator/allocation.h
@@ -4,4 +4,5 @@
 #include "./lifeinterval.h"
 
 void walkIntervals(IntervalList* unhandled);
+void MoveitFromAtoB(list_entry_t* A, list_entry_t* B, Interval* it);
 #endif
