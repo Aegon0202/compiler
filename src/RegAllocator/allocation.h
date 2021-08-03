c9ae0566956fd0d2e84fa460f080e30d547b7cfd
nicolasios <1498913522@qq.com>
2021-08-03 09:00:32 +0800

    allocate func

--- src/RegAllocator/allocation.h
+++ src/RegAllocator/allocation.h
@@ -3,8 +3,17 @@
 
 #include "./lifeinterval.h"
 
-void walkIntervals(IntervalList* unhandled);
-void MoveitFromAtoB(list_entry_t* A, list_entry_t* B, IntervalList* itList);
-int isCover(Interval* it, int position);
+#define MAX_NUM 1000
 
+//基本框架
+void walkIntervals(IntervalList* unhandled);
+
+//将itList从A移动到B
+void MoveitFromAtoB(list_entry_t* A, list_entry_t* B, IntervalList* itList);
+
+//判断是否it是否包含position
+int isCoverd(Interval* it, int position);
+
+//能否直接分配一个空闲的寄存器
+int tryAllocateFreeRegister(Interval* current, list_entry_t* active_list_head, list_entry_t* inactive_list_head);
 #endif
